f72cc7508878d780830700eb4b43d884e7d19828
muktadiranik <muktadiranik@gmail.com>
2024-10-25 23:21:01 +0600

    index.css

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Button } from "@chakra-ui/react";
 function App() {
   return (
     <div>
-      <Button variant="outline" colorPalette={"green"} size={"lg"}>
+      <Button variant="outline" colorPalette={"green"} size={"2xl"}>
         Solid
       </Button>
     </div>
